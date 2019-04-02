51decb0e487dafbecb08701d3843cb2320a67c5e
Mads Christian Lund <mcl@ihp.dk>
2019-04-02 14:15:17 +0200

    Initial implementation
    * Added support for explicit context
    * Added iterate(...) to ZMQChannel

--- src/ZMQChannels.jl
+++ src/ZMQChannels.jl
@@ -1,3 +1,91 @@
 module ZMQChannels
 
+using ZMQ
+import Base: put!, take!, isready, fetch, isopen, close, bind, finalize, iterate
+export put!, take!, isready, fetch, isopen, close, bind, iterate
+
+function put!(socket::Socket, value)
+    send(socket, value)
 end
+
+function take!(socket::Socket)
+    return recv(socket)
+end
+
+function isready(socket::Socket)
+    return true
+end
+
+
+export ZMQPullChannel, ZMQPushChannel
+
+mutable struct ZMQChannel
+    socket::Socket
+    function ZMQChannel(socket::Socket)
+        this = new()
+        this.socket = socket
+        return this
+    end
+end
+
+function ZMQPullChannel(context::Context, url::String)
+    channel = ZMQChannel(Socket(context, PULL))
+    bind(channel.socket, url)
+    return channel
+end
+function ZMQPullChannel(url::String)
+    return ZMQPullChannel(ZMQ.context(), url)
+end
+
+function ZMQPushChannel(context::Context, url::String)
+    channel = ZMQChannel(Socket(context, PUSH))
+    connect(channel.socket, url)
+    return channel
+end
+
+function ZMQPushChannel(url::String)
+    return ZMQPushChannel(ZMQ.context(), url)
+end
+
+
+function put!(channel::ZMQChannel, input)
+    send(channel.socket, input)
+end
+
+
+function take!(channel::ZMQChannel)
+    recv(channel.socket)
+end
+
+function iterate(channel::ZMQChannel, state::Any=nothing)
+    if (isopen(channel))
+        return (take!(channel), nothing)
+    end
+    return nothing
+end
+
+function isready(channel::ZMQChannel)
+    isready(channel.socket)
+end
+
+function fetch(channel::ZMQChannel)
+    fetch(channel.socket)
+end
+
+function close(channel::ZMQChannel)
+    close(channel.socket)
+end
+
+function isopen(channel::ZMQChannel)
+    isopen(channel.socket)
+end
+
+function bind(channel::ZMQChannel, task::Task)
+    bind(channel.socket, task)
+end
+
+function finalize(channel::ZMQChannel)
+    close(channel)
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,33 @@
 using Test
 
-@testset "ZMQChannels" begin
+import ZMQ
+import ZMQChannels
+
+@testset "Releasing port when closing channels" begin
+  @testset "ZMQPullChannel" begin
+      try
+        channel = ZMQChannels.ZMQPullChannel("tcp://*:6666")
+        close(channel)
+
+        sleep(0.1)
+
+        channel = ZMQChannels.ZMQPullChannel("tcp://*:6666")
+        close(channel)
+      catch e
+        @error "Should now throw any exceptions. Got: $e"
+        @test false
+      end
+  end
+
+  @testset "ZMQPushChannel" begin
+    try
+      channel = ZMQChannels.ZMQPushChannel("tcp://localhost:6666")
+      close(channel)
+      channel = ZMQChannels.ZMQPushChannel("tcp://localhost:6666")
+      close(channel)
+    catch e
+      @error "Should now throw any exceptions. Got: $e"
+      @test false
+    end
+  end
 end
